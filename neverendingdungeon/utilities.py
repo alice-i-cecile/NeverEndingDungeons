c111441c5e5b7d7c5b701c046262985b7df3506d
Alice Cecile <alice.i.cecile@gmail.com>
2018-02-14 18:52:55 -0500

    Properly parses tags

--- neverendingdungeon/utilities.py
+++ neverendingdungeon/utilities.py
@@ -8,14 +8,14 @@ def import_element(e_series):
                               gm_notes = e_series.gm_notes,
                               cr = e_series.cr,
                               size = e_series.size,
-                              tags = e_series.tags)
+                              tags = e_series.tags.split(sep=", "))
     else if e_series.element_type == 'Interactable':
         new_element = Element(name = e_series.name,
                               description = e_series.description,
                               gm_notes = e_series.gm_notes,
                               cr = e_series.cr,
                               size = e_series.size,
-                              tags = e_series.tags,
+                              tags = e_series.tags.split(sep=", "),
                               interaction_result = e_series.interaction_result)
     #TODO: add conversion of inventory to list
     else if e_series.element_type == 'NPC':
@@ -24,24 +24,23 @@ def import_element(e_series):
                               gm_notes = e_series.gm_notes,
                               cr = e_series.cr,
                               size = e_series.size,
-                              tags = e_series.tags,
+                              tags = e_series.tags.split(sep=", "),
                               race = e_series.race,
                               disposition = e_series.disposition,
-                              inventory = e_series.inventory
-                              )
+                              inventory = e_series.inventory)
     else if e_series.element_type == 'SkillCheck':
         new_element = Element(name = e_series.name,
                               description = e_series.description,
                               gm_notes = e_series.gm_notes,
                               cr = e_series.cr,
                               size = e_series.size,
-                              tags = e_series.tags,
+                              tags = e_series.tags.split(sep=", "),
                               ability = e_series.ability,
                               proficiency = e_series.proficency,
                               difficulty = e_series.difficulty,
                               success = e_series.success,
                               failure = e_series.failure)
     else:
-        raise ValueError(f"Invalid element_type {e_series.element_type
+        raise ValueError(f'Invalid element_type {e_series.element_type}')
 
     return new_element
