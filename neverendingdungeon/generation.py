22cdeb7a3ad519f7669aed2ba9f859a1ec73ed7e
Alice Cecile <alice.i.cecile@gmail.com>
2018-02-14 15:27:25 -0500

    Refactored how element generation works
    Now looks up from table, rather than generating piecewise.

--- neverendingdungeon/generation.py
+++ neverendingdungeon/generation.py
@@ -6,7 +6,7 @@ import random
 
 Dungeon = List[Room]
 
-# TODO: add support for subclasses
+# TODO: change to select_elements
 def generate_element(element_type: str = None,
                      description: str = None,
                      gm_notes: str = None,
@@ -71,16 +71,12 @@ def generate_element(element_type: str = None,
 def populate_room(room: Room,
                   xp_budget: int = 0,
                   gold_budget: int = 0,
-                  n_elements: int = None,
                   shape: List[Position] = None,
-                  connection_type: str = None,
-                  connection_location: int = None,
                   challenge: str = None,
-                  safety: str = None,
-                  flavour: str = None,
                   tags: Tags = []) -> Room:
-    """Adds content to a room. Decides theme, experience budget, and loot budget,
-        then adds suitable elements.
+    """Adds content to a room. The challenge of the room determines its safety
+        and relative xp_budget, then elements are selected that match the tags
+        to approximately match the budget of experience and gold.
 
     Args:
         room: The Room to work on.
@@ -88,6 +84,9 @@ def populate_room(room: Room,
             elements in the room.
         gold_budget: The budget for gold value to allocate between the items in
             the room.
+        shape: The vertices of the room.
+        challenge: The relative difficulty of the room.
+        tags: The thematic attributes of the room to sync elements to.
 
     Returns:
         A Room with completed attributes.
@@ -119,31 +118,81 @@ def populate_room(room: Room,
     if challenge is None:
         challenge = random.choice(valid_challenges)
     room.challenge = challenge
+    room.xp_budget *= challenge_multipliers[challenge]
 
     challenge_safety_mapping = {'Trivial': 'Safe',
                                 'Easy': 'Sheltered',
                                 'Medium': 'Risky',
                                 'Hard': 'Unsafe',
                                 'Deadly': 'Unsafe'}
-    if safety is None:
-        safety = challenge_safety_mapping[challenge]
-    room.safety = safety
 
-    if flavour is None:
-        room.flavour = random.choice(room_flavours)
-    room.flavour = flavour
+    room.safety = challenge_safety_mapping[challenge]
 
     if tags is []:
         tags = random.choice(universal_tags)
     room.tags.append(tags)
 
-    if n_elements is None:
-        n_elements = random.randrange(1,5)
+    # TODO: change to penalized optimization approach
+    n_elements = random.randrange(1,5)
 
-    room.elements = [generate_element() for _ in range(n_elements)]
+    # TODO: add tag, xp, constraints
+    # TODO: use select_elements approach
+    for i in range(n_elements):
+
+        selected_element = random.choice(elements_df.shape[0])
+        e_series = element_df.iloc[selected_element, ]
+
+        # TODO: is there a way to automatically apply matching names?
+        # TODO: move into seperate import_element function in utilities.py
+        if e_series.element_type == 'Base':
+            new_element = Element(name = e_series.name,
+                                  description = e_series.description,
+                                  gm_notes = e_series.gm_notes,
+                                  cr = e_series.cr,
+                                  size = e_series.size,
+                                  tags = e_series.tags)
+        else if e_series.element_type == 'Interactable':
+            new_element = Element(name = e_series.name,
+                                  description = e_series.description,
+                                  gm_notes = e_series.gm_notes,
+                                  cr = e_series.cr,
+                                  size = e_series.size,
+                                  tags = e_series.tags,
+                                  interaction_result = e_series.interaction_result)
+        #TODO: add conversion of inventory to list
+        else if e_series.element_type == 'NPC':
+            new_element = Element(name = e_series.name,
+                                  description = e_series.description,
+                                  gm_notes = e_series.gm_notes,
+                                  cr = e_series.cr,
+                                  size = e_series.size,
+                                  tags = e_series.tags,
+                                  race = e_series.race,
+                                  disposition = e_series.disposition,
+                                  inventory = e_series.inventory
+                                  )
+        else if e_series.element_type == 'SkillCheck':
+            new_element = Element(name = e_series.name,
+                                  description = e_series.description,
+                                  gm_notes = e_series.gm_notes,
+                                  cr = e_series.cr,
+                                  size = e_series.size,
+                                  tags = e_series.tags,
+                                  ability = e_series.ability,
+                                  proficiency = e_series.proficency,
+                                  difficulty = e_series.difficulty,
+                                  success = e_series.success,
+                                  failure = e_series.failure)
+        else:
+            raise ValueError(f"Invalid element_type {e_series.element_type}")
+
+        # TODO: generate within room bounds
+        element.location = (random.choice([0,1]), random.choice([0,1]))
+        room.elements.append(new_element)
 
     return room
 
+# TODO: set connection locations at this level
 def generate_dungeon_structure(n_rooms: int,
                                 layout: string = 'linear',
                                 **kwargs) -> Dungeon:
@@ -168,6 +217,7 @@ def generate_dungeon_structure(n_rooms: int,
 
     return rooms
 
+#TODO: set connection types at this level
 def generate_dungeon(n_rooms: int,
     party_level: int = 1, party_size: int = 4,
     layout='linear': string, **kwargs) -> Dungeon:
