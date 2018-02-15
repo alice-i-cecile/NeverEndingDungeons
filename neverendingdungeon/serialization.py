75e75fa41b767a4d09d3d914a12ebbcb009d1bfc
Alice Cecile <alice.i.cecile@gmail.com>
2018-02-15 12:40:39 -0500

    Added very crude svg map creation

--- neverendingdungeon/serialization.py
+++ neverendingdungeon/serialization.py
@@ -1,4 +1,26 @@
-from classes import *
-
-# TODO: add svg maps
 # TODO: add json dungeon saving
+
+from classes import *
+import generation
+import svgwrite
+
+
+# TODO: add grid lines
+def generate_svg_map(dungeon: generation.Dungeon):
+    dmap = svgwrite.Drawing()
+
+    # TODO: add connections
+    # TODO: add element locations
+    def draw_room(room: Room):
+        room_svg = svgwrite.container.Group()
+        abs_shape = room.shape + room.coord
+
+        walls_svg = svgwrite.Polygon(abs_shape)
+        return walls_svg
+
+    for r in Dungeon:
+        dmap.add(draw_room(r))
+
+    dmap.saveas("../output/dev_dungeonmap.svg")
+
+    return dmap
